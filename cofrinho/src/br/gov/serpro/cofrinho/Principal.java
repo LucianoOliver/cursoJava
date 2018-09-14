068b3de4f5048fec3d9eb22b1b239c6134dcaf88
LucianoOliver <luanrochaoliveira@gmail.com>
2018-09-13 21:31:50 -0300

    y
    y

--- cofrinho/src/br/gov/serpro/cofrinho/Principal.java
+++ cofrinho/src/br/gov/serpro/cofrinho/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Moeda moeda1 = new Moeda(0.10);
+		Moeda moeda1 = new Moeda(0.21);
 		Cofrinho cofrinho = new Cofrinho();
 		
 		Moeda moeda2 = new Moeda(0.10);
