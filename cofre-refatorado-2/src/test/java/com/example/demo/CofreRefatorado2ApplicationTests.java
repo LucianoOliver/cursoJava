29187a35d619e11e9619ead1cbae19fcc2081117
Luciano <luanrochaoliveira@gmail.com>
2018-09-18 22:30:44 -0300

    testes

--- cofre-refatorado-2/src/test/java/com/example/demo/CofreRefatorado2ApplicationTests.java
+++ cofre-refatorado-2/src/test/java/com/example/demo/CofreRefatorado2ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,20 @@ public class CofreRefatorado2ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Cofrinho cofrinho = new Cofrinho();
+		
+		Moeda moeda = new Moeda(ValorMoeda.CINCO_CENTAVOS);
+		cofrinho.adicionarMoeda(moeda);
+		assertEquals(1, cofrinho.calcularQuantidadeTotalMoedas(), 1);
+	}
+	
+	@Test
+	public void contextLoads2() {
+		Cofrinho cofrinho = new Cofrinho();
+		
+		Moeda moeda = new Moeda(ValorMoeda.CINCO_CENTAVOS);
+		cofrinho.adicionarMoeda(moeda);
+		assertEquals(1, cofrinho.calcularValorTotalMoedas(), 1);
 	}
 
 }
