db870591acfff17658509df51b5c4a2706fe586a
Luciano <luciano.oliveira@serpro.gov.br>
2018-09-27 17:02:22 -0300

    testes

---
+++ mocks-cap1/carrinho-de-compra/src/main/java/com/example/demo/CarrinhoDeCompraApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoDeCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoDeCompraApplication.class, args);
+	}
+}
---
+++ mocks-cap1/carrinho-de-compra/src/main/java/com/example/demo/carrinho/Cheque.java
@@ -0,0 +1,12 @@
+package com.example.demo.carrinho;
+
+public class Cheque {
+	
+	private String nomeBanco;
+	private Integer IDBanco;
+	
+	public boolean isAutorizado() {
+		return false;
+	}
+
+}
