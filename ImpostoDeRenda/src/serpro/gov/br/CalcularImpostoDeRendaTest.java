1b33db43fcf58714e649142bf09ef10a6531ee37
Luciano Oliveira <luciano.oliveira@serpro.gov.br>
2018-11-23 16:33:01 -0200

    imposto

--- ImpostoDeRenda/src/serpro/gov/br/CalcularImpostoDeRendaTest.java
+++ ImpostoDeRenda/src/serpro/gov/br/CalcularImpostoDeRendaTest.java
@@ -20,13 +20,20 @@ public class CalcularImpostoDeRendaTest {
 		listaConstribuintes.add(new PessoaJuridica("Supermercados B",2000000.00,"10066"));
 		listaConstribuintes.add(new PessoaFisica("Carla Maia",1500.00,"33333"));
 		listaConstribuintes.add(new PessoaJuridica("Posto XX",500000.00,"10077"));
-		listaConstribuintes.add(new PessoaFisica("José da Silva",95000.00,"97687"));
+		listaConstribuintes.add(new PessoaFisica("Jose da Silva",95000.00,"97687"));
 		
 		
 		for (Contribuinte contribuinte : listaConstribuintes) {
 			resultados.add(contribuinte.calcularImposto());
 		}
 		
+		assertEquals(3000.00 * 1.10, resultados.get(0) ,0000.1);
+		assertEquals(150000.00 * 1.10, resultados.get(1) ,0000.1);
+		assertEquals(5000.00 * 1.15, resultados.get(2) ,0000.1);
+		assertEquals(2000000.00 * 1.10, resultados.get(3) ,0000.1);
+		assertEquals(1500.00 * 0, resultados.get(4) ,0000.1);
+		assertEquals(500000.00 * 1.10, resultados.get(5) ,0000.1);
+		assertEquals(95000.00 * 1.30, resultados.get(6) ,0000.1);
 		//Assert de cada resultado
 	}
 
--- ImpostoDeRenda/src/serpro/gov/br/PessoaFisica.java
+++ ImpostoDeRenda/src/serpro/gov/br/PessoaFisica.java
@@ -11,7 +11,45 @@ public class PessoaFisica extends Contribuinte {
 
 	@Override
 	public Double calcularImposto() {
-		return valor * 1.10;
+		
+		impostoZero();
+		impostoDezPorcento();
+		impostoQuinzePorcento();
+		impostoVinteCincoPorCento();
+		
+		return impostoTrintaporcento();
+	}
+
+	private Double impostoTrintaporcento() {
+		return valor * 1.30;
+	}
+
+	private Double impostoVinteCincoPorCento() {
+		if(valor >= 6000.01 && valor <= 8000.0) {
+			return valor *1.25;
+		}
+		return valor;
+	}
+
+	private Double impostoQuinzePorcento() {
+		if(valor >= 4000.01 && valor <= 6000.0) {
+			return valor *1.15;
+		}
+		return valor;
+	}
+
+	private Double impostoDezPorcento() {
+		if(valor >= 2000.01 && valor <= 4000.0) {	
+			return valor *1.10;
+		}
+		return valor;
+	}
+
+	private Double impostoZero() {
+		if(valor >= 0 && valor <= 2000.0) {
+			return valor * 0;
+		}
+		return valor;
 	}
 
 	
